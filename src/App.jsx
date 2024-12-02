793ca65f7f5f82b7abbee14041773fcfbb4caaf3
Duong Hoai Bao <dhbao2505@gmail.com>
2024-12-02 12:03:18 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { DatePicker } from 'antd';
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+      <DatePicker />
+    </h1>
+  )
+}
+
+export default App
